18bdd9e70dce31b70fe9fe572787347381dbe41f
Kapil <kapil.lamichhane@capitalsigns.com>
2019-04-28 15:21:21 -0400

    This should be it

--- SoftGreen.py
+++ SoftGreen.py
@@ -1,28 +1,49 @@
-from flask import Flask, render_template, request, abort, session, redirect, url_for
-from models import Person, Administrator, Faculty, Moderator, Semester, Courses
+from flask import Flask, render_template, request, session, redirect, url_for, flash, abort
+from models import Administrator,Moderator, Faculty, SelectedCourseInfo, Semester, Courses, ClassRoom, DayTime, db
 from constants import ROLES
+
+import flask_admin as admin
+from flask_admin.contrib.peewee import ModelView
+
 app = Flask(__name__)
 app.secret_key = "SomeAwesomeSecret"
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+
+admin = admin.Admin(app, name="SoftGreen")
+
+admin.add_view(ModelView(Administrator))
+admin.add_view(ModelView(Moderator))
+admin.add_view(ModelView(Faculty))
+admin.add_view(ModelView(SelectedCourseInfo))
+admin.add_view(ModelView(Semester))
+admin.add_view(ModelView(Courses))
+admin.add_view(ModelView(DayTime))
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index_login():
     logged_status = session.get("logged_in")
-    return render_template('options_to_login.html')
+    if not logged_status:
+        return render_template('options_to_login.html')
+    else:
+        return redirect(url_for("faculty_selection"))
 
 
 def login(user_id, pin_number, user_role):
     if user_role == ROLES.get("admin"):
-        admin = Administrator.get(Administrator.user_id == user_id)
-        if admin and (admin.pin_number == pin_number):
+        administrator = Administrator.get_or_none(Administrator.user_id == user_id)
+        if administrator and (administrator.pin_number == pin_number):
+            session['user_id'] = administrator.id
             return True
     elif user_role == ROLES.get("moderator"):
-        moderator = Moderator.get(Moderator.user_id == user_id)
+        moderator = Moderator.get_or_none(Moderator.user_id == user_id)
         if moderator and (moderator.pin_number == pin_number):
+            session['user_id'] = moderator.id
             return True
     elif user_role == ROLES.get("faculty"):
-        faculty = Faculty.get(Faculty.user_id == user_id)
-        if faculty and (faculty.pin_number == pin_number):
+        faculty = Faculty.get_or_none(Faculty.user_id == user_id)
+        if faculty and(faculty.pin_number == pin_number):
+            session['user_id'] = faculty.id
             return True
     else:
         return False
@@ -43,6 +64,11 @@ def administrator_login():
         if logged_in:
             session['logged_in'] = True
             session['role'] = "admin"
+            flash("You have successfully logged in.")
+            return redirect(url_for("admin.index"))
+        else:
+            flash("Login in failed. Please try again.")
+            return redirect(url_for('index_login'))
 
     return render_template("admin_login.html")
 
@@ -53,11 +79,14 @@ def faculty_login():
         form_user = int(request.form.get('user-id'))
         form_pin = int(request.form.get('user-pin'))
         logged_in = login(form_user, form_pin, user_role=ROLES.get("faculty"))
-        print("logged in", logged_in)
         if logged_in:
             session['logged_in'] = True
             session['role'] = "faulty"
+            flash("You have successfully logged in.")
             return redirect(url_for("faculty_selection"))
+        else:
+            flash("Login in failed. Please try again.")
+            return redirect(url_for('index_login'))
 
     return render_template("faculty_login.html")
 
@@ -71,7 +100,18 @@ def moderator_login():
         if logged_in:
             session['logged_in'] = True
             session['role'] = "moderator"
+            flash("You have successfully logged in.")
+            return redirect(url_for("moderator_view"))
+        else:
+            flash("Login in failed. Please try again.")
+            return redirect(url_for('index_login'))
     return render_template("moderator_login.html")
+
+
+@app.route('/moderator_view', methods=['POST', 'GET'])
+def moderator_view():
+    to_review = SelectedCourseInfo.select()
+    return render_template("moderator_view.html", to_review = to_review)
 
 
 @app.route('/faculty_selection', methods=['POST', 'GET'])
@@ -79,44 +119,43 @@ def faculty_selection():
     if request.method == "POST":
         form_term = request.form.get("term_id")
         form_courses = request.form.getlist("course_ids")
+        return redirect(url_for("day_time_selection", selected_term=form_term, selected_courses=form_courses))
 
-        selected_term = Semester.get(id=form_term)
-        selected_courses = []
-        for item in form_courses:
-            selected_courses.append(Courses.get(id=item))
-        weekday = {
-            "Monday": 1,
-            "Tuesday": 2,
-            "Wednesday": 3,
-            "Thursday": 4,
-            "Friday": 5,
-            "Saturday": 6
-        }
-        teaching_hours = {
-            "7:30": 1,
-            "8:30": 2,
-            "9:30": 3,
-            "10:30": 4,
-            "11:30": 5,
-            "12:30": 6,
-            "13:30": 7,
-            "14:30": 8,
-            "15:30": 9,
-            "16:30": 10,
-            "17:30": 11,
-            "18:30": 12
-
-        }
-        return render_template(
-            "select_days_and_time.html",
-            selected_courses=selected_courses,
-            selected_term=selected_term,
-            weekday=weekday,
-            teaching_hours=teaching_hours
-        )
     courses = Courses.select()
     semester = Semester.select()
     return render_template("selection.html", courses=courses, semester=semester)
+
+
+@app.route('/select_day_time', methods=['POST', 'GET'])
+def day_time_selection():
+    if request.method == "POST":
+        final_selected = request.form.getlist("selected_course")
+        session_user = session.get("user_id")
+
+        for item in final_selected:
+            new_record = SelectedCourseInfo.create(faculty_id=session_user, course_id=item)
+            try:
+                with db.atomic():
+                    new_record.save()
+                    print(new_record)
+            except Exception as e:
+                flash("Something went wrong.")
+                return redirect(url_for("day_time_selection"))
+
+        flash("Your selected course has been sent to Moderator for review")
+        return redirect(url_for("faculty_selection"))
+
+    if request.method == "GET":
+
+        selected_courses = []
+        for item in request.args.getlist("selected_courses"):
+            selected_courses.append(Courses.get(id=item))
+        selected_term = Semester.get(id=request.args['selected_term'])
+        return render_template(
+            "select_days_and_time.html",
+            selected_term=selected_term,
+            selected_courses=selected_courses
+        )
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -3,14 +3,21 @@ from peewee import *
 db = SqliteDatabase('softgreen.db')
 
 
-class Person(Model):
-    first_name = CharField()
-    last_name = CharField()
-    user_id = IntegerField()
-    pin_number = IntegerField()
-
+class BaseModel(Model):
     class Meta:
         database = db
+
+
+class Person(BaseModel):
+    first_name = CharField()
+    last_name = CharField()
+    user_id = IntegerField(unique=True)
+    pin_number = IntegerField()
+    FT_PT = (
+        (1, "FT"),
+        (2, "PT")
+    )
+    ft_pt = IntegerField(choices=FT_PT)
 
 
 class Administrator(Person):
@@ -25,20 +32,37 @@ class Moderator(Person):
     pass
 
 
-class Semester(Model):
+class ClassRoom(BaseModel):
+    room = CharField(unique=True)
+
+
+class Semester(BaseModel):
     terms = CharField(unique=True)
 
-    class Meta:
-        database = db
+
+class Courses(BaseModel):
+    course_code = CharField()
+    course_name = CharField()
+    course_section = IntegerField()
+
+    credit = IntegerField()
+
+    notes = CharField(null=True)
+    tom_notes = CharField(null=True)
 
 
-class Courses(Model):
-    course_code = CharField(unique=True)
-    course_name = CharField(unique=True)
-    department = CharField()
+class DayTime(BaseModel):
+    courses = ForeignKeyField(Courses, backref="day_time")
+    days = CharField()
+    start_time = CharField()
+    end_time = CharField()
+    class_room = ManyToManyField(ClassRoom)
 
-    class Meta:
-        database = db
+
+class SelectedCourseInfo(BaseModel):
+    faculty = ForeignKeyField(Faculty, backref="selected_course")
+    course = ForeignKeyField(Courses, backref="selected_course")
+
 
 db.connect()
-db.create_tables([Person, Administrator, Faculty, Moderator, Semester, Courses])
+db.create_tables([Person, Administrator, Faculty, Moderator, Semester, Courses, DayTime, ClassRoom, SelectedCourseInfo])
